bb88cffb465c9a0f5ade02c4104cb84ad0f2819b
Brad Zacher <brad.zacher@gmail.com>
2022-12-09 09:42:01 +1030

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    '@typescript-eslint/semi': 'error',
+  }
+};
---
+++ foo.ts
@@ -0,0 +1,6 @@
+import {x} from './bar';
+import * as fs from 'fs';
+
+export const y = x + 1
+
+export const z = fs.constants.F_OK
